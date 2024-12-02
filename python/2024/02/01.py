039eb40e2c3950dee55549c6e4b744fb0194707e
Bodor Barnabás <bodor.barnabas@gmail.com>
2024-12-02 20:39:35 +0100

    Update 01.py

--- python/2024/02/01.py
+++ python/2024/02/01.py
@@ -10,16 +10,17 @@ reports = []
 for line in lines:
     report = [int(i) for i in line.split()]
     safe = True
-    inc = (report[0] - report[1]) > 0
-    print(inc)
+    print(report)
+    inc = report[0] < report[1]
     for j in range(1, len(report)):
         print(f"{report[j-1]}, {report[j]}")
         diff = report[j-1] - report[j]
-        if inc and diff < 0 and j != 1 or not inc and diff > 0 and j!= 1:
+        print(diff)
+        if not (diff >= 0 and diff <= 3) and j!= 1:
             safe = False
             break
-        if  diff < 1 and diff > 3:
-            safe = False
+    print(f"aaa {safe}")
     reports.append(safe)
 
 print(reports)
+
