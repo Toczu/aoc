533be80ca2d6b8dcdb0561fd7547e805552e3c90
Toczu6 <bodor.barnabas@gmail.com>
2024-12-07 21:09:03 +0100

    add day 5 first puzzle

--- python/2024/04/02.py
+++ python/2024/04/02.py
@@ -1,9 +1,32 @@
 import pathlib
+import numpy as np
 
 directory = str(pathlib.Path(__file__).parent.resolve()) + "/"
 
-file = open(directory + "puzzle_test.txt", "r")
+file = open(directory + "puzzle.txt", "r")
 lines = file.readlines()
 
+# M.S  -1,-1   -1, +1
+# .A.        X
+# M.S  +1,-1   +1, +1
+
+m = []
+
+# vertical
 for line in lines:
-    print(line.strip())
+    l = line.strip()
+    m.append(list(l))
+
+sum = 0
+m = np.array(m)
+for k in range(4):
+    for i in range(1, len(m) -1):
+        for j in range(1, len(m[:,]) - 1):
+            # print(i, j)
+            # print(m[i][j], m[i-1][j-1], m[i-1][j+1])
+            if m[i][j] == 'A' and m[i-1][j-1] == 'M' and m[i-1][j+1] == 'S' and m[i+1][j-1] == 'M' and m[i+1][j+1] == 'S':
+                sum += 1
+                # print('asd')
+    m = np.rot90(m)
+            
+print(sum)
--- python/2024/05/01.py
+++ python/2024/05/01.py
@@ -1,9 +1,57 @@
 import pathlib
+import numpy as np
 
 directory = str(pathlib.Path(__file__).parent.resolve()) + "/"
 
 file = open(directory + "puzzle_test.txt", "r")
 lines = file.readlines()
 
+rules = dict()
+updates = []
+change = False
+
 for line in lines:
-    print(line.strip())
+    if line == "\n":
+        change = True
+        continue
+    
+    if not change:
+        l = [int(i) for i in line.strip().split('|')]
+        key = l[0]
+        if key in rules.keys():
+            rules[key].append(l[1])
+        else:
+            rules.update({ l[0]: [l[1]] })
+    else:
+        updates.append([int(i) for i in line.strip().split(',')])
+
+# [print(f"{k}: {v}") for k,v in rules.items()]
+# [print(update) for update in updates]
+
+correct_updates = []
+for j in range(len(updates)):
+    update = updates[j]
+    correct = True
+    # print(f'-- checking line {update} --')
+    for i in range(len(update)):
+        page = update[i]
+        if page not in rules.keys():
+            continue
+        page_rules = rules[page]
+        # print(page_rules)
+        # print(np.isin(update, page_rules))
+        # print(np.any(np.isin(update, page_rules)))
+        # print(f"page {page}")
+        # print(f"before {update[:i]}")
+        # print(f"after {update[i+1:]}")
+        if np.any(np.isin(update[:i], page_rules)):
+            correct = False
+    if correct:
+        correct_updates.append(j)
+
+sum = 0
+for i in correct_updates:
+    middle = int(len(updates[i]) / 2)
+    sum += updates[i][middle]
+
+print(sum)
